6ef498d1089817767dd45c63196548508162fea4
Julien Hatin <julien@oversimple.fr>
2014-04-14 18:28:53 +0200

    Initial commit

---
+++ NfcNDEFReader/src/fr/oversimple/nfc/urlReader/MainActivity.java
@@ -0,0 +1,55 @@
+package fr.oversimple.nfc.urlReader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements NfcUrlReaderListener{
+
+	 private TextView demoTextView;
+	 private NfcUrlReader nfcUrlReader;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		demoTextView = (TextView) findViewById(R.id.demoTextview);
+		
+        nfcUrlReader = new NfcUrlReader(this);
+        
+		if(!nfcUrlReader.isNfcAvailableOnDevice()) {
+			Toast.makeText(this, "Nfc is not available on this device", Toast.LENGTH_SHORT).show();
+		}
+		
+		if(!nfcUrlReader.isNfcActivated()) {
+			Toast.makeText(this, "Nfc is not activated, please activate it", Toast.LENGTH_SHORT).show();
+		}
+		nfcUrlReader.setNfcUrlReaderListener(this);
+		nfcUrlReader.handleIntent(getIntent());
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		nfcUrlReader.setupForegroundDispatch(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		nfcUrlReader.stopForegroundDispatch(this);
+		super.onPause();
+	}
+	
+	@Override
+	protected void onNewIntent(Intent intent) {
+		nfcUrlReader.handleIntent(intent);
+	}
+
+	@Override
+	public void ndefDataRead(String ndefData) {
+		demoTextView.setText(ndefData);
+	}
+}
---
+++ NfcNDEFReader/src/fr/oversimple/nfc/urlReader/NfcUrlReaderListener.java
@@ -0,0 +1,5 @@
+package fr.oversimple.nfc.urlReader;
+
+public interface NfcUrlReaderListener {
+	public void ndefDataRead(String ndefData);
+}
